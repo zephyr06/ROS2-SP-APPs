fc6ebcb7db091082078a6c90c5a334e39b918018
zephyr06 <sen.zephyr.wang@gmail.com>
2024-02-18 15:45:15 -0500

    add ros2_commands.sh

--- ros2_commands.sh
+++ ros2_commands.sh
@@ -1,5 +1,10 @@
 # Build a single package
+colcon build --packages-select cpp_pubsub --allow-overriding cpp_pubsub
 colcon build --packages-select tsp_solver --allow-overriding tsp_solver
+. install/setup.bash
 
 # Build all packages
 colcon build 
+
+# Run the publiser node
+ros2 run cpp_pubsub talker
