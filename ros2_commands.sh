8101e25bad7a080394cde9574748024b6c1cffea
Dong Li <dongli@vt.edu>
2024-02-27 16:20:29 -0500

    add more commands

--- ros2_commands.sh
+++ ros2_commands.sh
@@ -1,6 +1,7 @@
 # Build a single package
 colcon build --packages-select cpp_pubsub --allow-overriding cpp_pubsub
 colcon build --packages-select tsp_solver --allow-overriding tsp_solver
+colcon build --packages-select mpc --allow-overriding mpc
 . install/setup.bash
 
 # Build all packages
