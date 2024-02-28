960a188c0cdb8fd5a467488b83d0918def2cd468
zephyr06 <sen.zephyr.wang@gmail.com>
2024-02-27 21:24:37 -0500

    update tsp listener

--- src/tsp_solver/src/listener_tsp.cpp
+++ src/tsp_solver/src/listener_tsp.cpp
@@ -1,63 +1,16 @@
-// Copyright 2016 Open Source Robotics Foundation, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// #include <functional>
-#include <memory>
-
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#pragma once
+#include "listener_base.h"
 #include "TSPSolver.h"
-#include "profiler.h"
-
-using std::placeholders::_1;
-class AppBase
-{
-  AppBase(std::string app_name):app_name_(app_name){}
-  virtual void run(){;}
-  // data member
-  std::string app_name_;
-};
-template<typename AppBase>
-class SubscriberAppBase : public rclcpp::Node
+class TSPApp : public AppBase
 {
 public:
-  SubscriberAppBase()
-  : Node("minimal_subscriber"),  start_time_(CurrentTimeInProfiler), target_profile_data_file_path_(getTimeRecordFolder()+app_name_+"_subscriber.txt")
+  TSPApp():AppBase("tsp"){}
+  void run() override
   {
-    subscription_ = this->create_subscription<std_msgs::msg::String>(
-      getTopicName(app_name_), 1, std::bind(&SubscriberAppBase::topic_callback, this, _1));
+    callTSP();
   }
 
-private:
-  void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
-  {
-    RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
-    // callTSP();
-    app_.run();
-    double current_time=getDuration(start_time_, CurrentTimeInProfiler);
-    std::string receive_message ="Receiving "+app_name_+" message:"+msg->data;
-    write_current_time_to_file(target_profile_data_file_path_, current_time, receive_message);
-  }
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
-
-  // data related to profiler
-  AppBase app_;
-  std::string app_name_;
-  TimerType start_time_;
-  std::string target_profile_data_file_path_;
 };
-
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
