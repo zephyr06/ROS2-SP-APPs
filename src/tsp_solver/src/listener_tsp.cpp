adb4eb61a95504324571546eb9fce6b0c6d8abed
zephyr06 <sen.zephyr.wang@gmail.com>
2024-02-27 21:22:35 -0500

    update tsp listener main

--- src/tsp_solver/src/listener_tsp.cpp
+++ src/tsp_solver/src/listener_tsp.cpp
@@ -61,7 +61,7 @@ private:
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<SubscriberAppBase>());
+  rclcpp::spin(std::make_shared<SubscriberAppBase<TSPApp>>());
   rclcpp::shutdown();
   return 0;
 }
