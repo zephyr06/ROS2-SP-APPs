3d3a33bf07043f55f556de4ea168dd557dec19d0
zephyr <sen.zephyr.wang@gmail.com>
2024-02-29 16:59:27 -0500

    add more to run_all_tasks

--- scripts/run_all_tasks.sh
+++ scripts/run_all_tasks.sh
@@ -4,3 +4,6 @@ source install/local_setup.bash
 
 python clear_all_time_records.py
 ros2 launch launch_all_the_nodes.py
+
+# python analyze_response_time.py
+# ./~/workspace/sdcard/SP_Metric_Opt/analyze_sp.sh
