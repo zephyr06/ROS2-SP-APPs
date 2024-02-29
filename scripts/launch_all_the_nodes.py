4fa0c0d11f04016cdb45caeef6823736c8ec4815
zephyr <sen.zephyr.wang@gmail.com>
2024-02-29 16:28:43 -0500

    update launch file

--- scripts/launch_all_the_nodes.py
+++ scripts/launch_all_the_nodes.py
@@ -13,49 +13,53 @@ def generate_launch_description():
             package='cpp_pubsub',
             namespace='',
             executable='talker',
-            name='cpp_pubsub',
-            arguments=["mpc", mpc_period]
+            name='talker_tsp',
+            arguments=["mpc", str(mpc_period)]
         ),
         Node(
             package='cpp_pubsub',
             namespace='',
             executable='talker',
-            name='cpp_pubsub',
-            arguments=["rrt", rrt_period]
+            name='talker_rrt',
+            arguments=["rrt", str(rrt_period)]
         ),
         Node(
             package='cpp_pubsub',
             namespace='',
             executable='talker',
-            name='cpp_pubsub',
-            arguments=["slam", slam_period]
+            name='talker_slam',
+            arguments=["slam", str(slam_period)]
         ),
         Node(
             package='cpp_pubsub',
             namespace='',
             executable='talker',
-            name='cpp_pubsub',
-            arguments=["tsp", tsp_period]
+            name='talker_tsp',
+            arguments=["tsp", str(tsp_period)]
         ),
         #  *************** Listeners ********************
         Node(
             package='mpc',
             name='mpc',
+            namespace='',
             executable='listener_mpc'
         ),
         Node(
             package='rrt_solver',
             name='rrt_solver',
+            namespace='',
             executable='rrt_listener'
         ),
         Node(
             package='dynaslam',
             name='dynaslam',
+            namespace='',
             executable='listener_slam'
         ),
         Node(
             package='tsp_solver',
             name='tsp_solver',
+            namespace='',
             executable='tsp_solver_listener'
         ),
     ])
