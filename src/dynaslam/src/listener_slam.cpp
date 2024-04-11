8c09024a4739fbba1c8e8a86ebb2466933388c23
Dong Li <dongli@vt.edu>
2024-04-11 16:55:18 -0400

    fix bug, dynaslam will process the frame according to the input message published by the talker

--- src/dynaslam/src/listener_slam.cpp
+++ src/dynaslam/src/listener_slam.cpp
@@ -4,7 +4,7 @@
 class SLAMApp : public AppBase {
  public:
   SLAMApp() : AppBase("slam") { slam_wrapper_.init(); }
-  void run(int msg_cnt) override { slam_wrapper_.next(int msg_cnt); }
+  void run(int msg_cnt) override { slam_wrapper_.next(msg_cnt); }
   DynaSLAMWrapperForROS2 slam_wrapper_;
 };
 int main(int argc, char* argv[]) {
