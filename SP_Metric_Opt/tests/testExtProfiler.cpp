8388653308112fb4ff401a4837e62e9f5459e063
zephyr <sen.zephyr.wang@gmail.com>
2024-04-26 17:36:32 -0400

    add testExtProfiler

--- SP_Metric_Opt/tests/testExtProfiler.cpp
+++ SP_Metric_Opt/tests/testExtProfiler.cpp
@@ -9,15 +9,19 @@ using namespace std;
 using namespace GlobalVariables;
 TEST(BasicExample, v1) {
     ExecutionTimeProfiler ext_profiler;
-    ext_profiler.start();
-    int a = 1;
-    for (int i = 0; i < 1e9; i++) {
-        for (int j = 0; j < 2; j++) a++;
-        int b = a * 2;
-        a = b * 2;
+    for (int k = 0; k < 5; k++) {
+        ext_profiler.start();
+        int a = 1;
+        for (int i = 0; i < 1e9; i++) {
+            a++;
+            a++;
+            int b = a * 2;
+            a = b * 2;
+        }
+        ext_profiler.end();
+        cout << "Execution time: " << ext_profiler.get_exe_time()
+             << " seconds\n";
     }
-    ext_profiler.end();
-    cout << "Execution time: " << ext_profiler.get_exe_time() << " seconds\n";
 }
 
 int main(int argc, char **argv) {
