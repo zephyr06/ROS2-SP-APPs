5a269b5ece6f772e7a731c8143b9a348a26c4bcc
zephyr06 <sen.zephyr.wang@gmail.com>
2024-04-27 13:05:27 -0400

    add PeriodicReleaser

--- SP_Metric_Opt/tests/testPublisher.cpp
+++ SP_Metric_Opt/tests/testPublisher.cpp
@@ -6,12 +6,56 @@
 #include <iomanip>
 #include <iostream>
 
-class PeriodicReleaser {
-    void release(double period_ms) {}
+#include "gmock/gmock.h"  // Brings in gMock.
+#include "sources/Utils/profilier.h"
+
+class AppBase {
+   public:
+    AppBase(std::string app_name) : app_name_(app_name) {}
+    // function arguments not used for now
+    virtual void run(int) { ; }
+
+    // data member
+    std::string app_name_;
 };
 
-namespace bpt = boost::posix_time;
-namespace asio = boost::asio;
+template <typename AppBase>
+class PeriodicReleaser {
+   public:
+    PeriodicReleaser(int period_ms, int release_total, const AppBase& app)
+        : app_(app),
+          period_ms_(period_ms),
+          release_total_left_(release_total) {}
+
+    void caller(const boost::system::error_code&,
+                boost::asio::deadline_timer& t) {
+        if (release_total_left_ == 0)
+            return;
+        app_.run(0);
+        release_total_left_--;
+        t.expires_at(t.expires_at() +
+                     boost::posix_time::milliseconds(period_ms_));
+        // if (++count < 100)
+        t.async_wait(boost::bind(&PeriodicReleaser<AppBase>::caller, this,
+                                 boost::asio::placeholders::error,
+                                 boost::ref(t)));
+    }
+
+    void release() {
+        boost::asio::io_service io;
+        boost::asio::deadline_timer t(
+            io, boost::posix_time::milliseconds(period_ms_));
+        t.async_wait(boost::bind(&PeriodicReleaser<AppBase>::caller, this,
+                                 boost::asio::placeholders::error,
+                                 boost::ref(t)));
+        io.run();
+    }
+
+    AppBase app_;
+    int period_ms_;
+    int release_total_left_;
+};
+
 void busySpinForSeconds(int ms) {
     auto startTime = std::chrono::high_resolution_clock::now();
     auto endTime = startTime + std::chrono::milliseconds(ms);
@@ -20,32 +64,25 @@ void busySpinForSeconds(int ms) {
         // Busy spin
     }
 }
-int i = 0;
-void fun() {
-    std::cout << "Called at " << bpt::microsec_clock::local_time().time_of_day()
-              << '\n';
-    // sleep(1.5);
-    if (i % 2 == 0)
-        busySpinForSeconds(1500);
-    else
-        busySpinForSeconds(500);
-    i++;
+
+class AppTest : public AppBase {
+   public:
+    AppTest() : AppBase("AppTest") {}
+    void run(int) override { std::cout << "Run one time!\n"; }
+};
+
+TEST(PeriodicReleaser, v1) {
+    AppTest app;
+    PeriodicReleaser<AppTest> releaser(100, 5, app);
+    auto start = std::chrono::high_resolution_clock::now();
+    releaser.release();
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> duration = end - start;
+    EXPECT_NEAR(100 * (5 + 1), duration.count(), 5e-1);
 }
 
-void caller(const boost::system::error_code&, asio::deadline_timer& t,
-            int& count) {
-    fun();
-    t.expires_at(t.expires_at() + bpt::seconds(1));
-    if (++count < 100)
-        t.async_wait(boost::bind(caller, asio::placeholders::error,
-                                 boost::ref(t), boost::ref(count)));
-}
-
-int main() {
-    asio::io_service io;
-    asio::deadline_timer t(io, bpt::seconds(1));
-    int count = 0;
-    t.async_wait(boost::bind(caller, asio::placeholders::error, boost::ref(t),
-                             boost::ref(count)));
-    io.run();
+int main(int argc, char** argv) {
+    // ::testing::InitGoogleTest(&argc, argv);
+    ::testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
 }
