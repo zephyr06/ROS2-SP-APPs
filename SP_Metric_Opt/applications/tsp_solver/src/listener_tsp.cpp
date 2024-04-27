3f02d5e70e37f7089c9ab36d624a26e0783a1803
zephyr06 <sen.zephyr.wang@gmail.com>
2024-04-27 15:46:37 -0400

    update tsp listener

--- SP_Metric_Opt/applications/tsp_solver/src/listener_tsp.cpp
+++ SP_Metric_Opt/applications/tsp_solver/src/listener_tsp.cpp
@@ -8,16 +8,19 @@
 class TSPApp : public AppBase {
    public:
     TSPApp() : AppBase("tsp") {}
-    void run(int msg_cnt) override { callTSP(); }
+    void run(int) override { callTSP(); }
 };
 
 int main(int argc, char* argv[]) {
     SP_OPT_PA::DAG_Model dag_tasks = SP_OPT_PA::ReadDAG_Tasks(
         GlobalVariables::PROJECT_PATH +
         "../all_time_records/task_characteristics.yaml");
-    assert(dag_tasks.tasks[0].name == "TSP");
-    double period = dag_tasks.tasks[0].period;
-    int count = dag_tasks.tasks[0].total_running_time / period;
+
+    int index = 0;
+
+    assert(dag_tasks.tasks[index].name == "TSP");
+    double period = dag_tasks.tasks[index].period;
+    int count = dag_tasks.tasks[index].total_running_time / period;
     TSPApp app;
     PeriodicReleaser<TSPApp> releaser(period, count, app);
     releaser.release();
