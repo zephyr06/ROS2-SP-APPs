27dfe27ce1a977dcad0bc4bc6acb9772032f976f
zephyr06 <sen.zephyr.wang@gmail.com>
2024-04-30 11:08:36 -0400

    fix scheduler bug

--- SP_Metric_Opt/applications/real_time_manager/src/listener_scheduler.cpp
+++ SP_Metric_Opt/applications/real_time_manager/src/listener_scheduler.cpp
@@ -116,7 +116,7 @@ int main(int argc, char *argv[]) {
         std::cerr<<"Wrong arg format: example usage ./scheduler_runner optimizerBF 30000 The last argument is period of the scheduler in miliseconds\n";
     int period = std::stoi(argv[2]);
     int count = dag_tasks.tasks[0].total_running_time / period;
-    SchedulerApp app;
+    SchedulerApp app(argc, argv);
     PeriodicReleaser<SchedulerApp> releaser(period, count, app);
     releaser.release();
     return 0;
