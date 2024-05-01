17fc92c05e35c334dae56801b5d10d13a6dd8211
zephyr06 <sen.zephyr.wang@gmail.com>
2024-05-01 14:09:29 -0400

    update calculate avg et

--- SP_Metric_Opt/Visualize_SP_Metric/calculate_average_et.py
+++ SP_Metric_Opt/Visualize_SP_Metric/calculate_average_et.py
@@ -5,12 +5,12 @@ def get_ext_list(file_name):
     ext_list = []
     with open(file_name, 'r') as f:
         for line in f:
-            ext_str = line.split("::")[1][:-1]  
+            ext_str = line.split("::")[2][:-1] 
             ext_list.append(float(ext_str))
     return ext_list
 
 if __name__ == "__main__":
-    file_name = os.path.join(os.path.dirname(OPT_SP_PROJECT_PATH), "all_time_records", "slam_execution_time.txt")
+    file_name = os.path.join(os.path.dirname(OPT_SP_PROJECT_PATH), "all_time_records", "SLAM_execution_time.txt")
     # file_name = os.path.join(os.path.dirname(OPT_SP_PROJECT_PATH), "Experiments", "slam_execution_time_bf.txt")
     # file_name = os.path.join(os.path.dirname(OPT_SP_PROJECT_PATH), "Experiments", "slam_execution_time_rm_slam_high.txt")
     # file_name = os.path.join(os.path.dirname(OPT_SP_PROJECT_PATH), "Experiments", "slam_execution_time_rm_slam_low.txt")
