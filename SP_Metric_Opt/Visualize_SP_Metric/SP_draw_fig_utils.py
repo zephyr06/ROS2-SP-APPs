38e4f051585ff349003603081149af7f69fc5b48
zephyr <sen.zephyr.wang@gmail.com>
2024-05-02 17:05:41 -0400

    add function draw_and_saveSP_fig_single_run

--- SP_Metric_Opt/Visualize_SP_Metric/SP_draw_fig_utils.py
+++ SP_Metric_Opt/Visualize_SP_Metric/SP_draw_fig_utils.py
@@ -192,3 +192,31 @@ def get_sp_value_list(tasks_name_list, tasks_name_to_info, horizon, horizon_gran
             a=1
         sp_value_list.append(sp_value)
     return sp_value_list
+
+
+def draw_and_saveSP_fig_single_run(data_folder_paths, discard_early_time, horizon_granularity, horizon):
+
+    task_set_config = os.path.join(
+        os.path.dirname(OPT_SP_PROJECT_PATH),"all_time_records", "task_characteristics.yaml")
+    verify_task_set_config(task_set_config)
+    app_name2period = get_app2period(task_set_config)
+    tasks_name_list = ['TSP', 'RRT', 'SLAM', 'MPC']
+
+
+    for method_name, data_folder_path in data_folder_paths.items():
+        tasks_name_to_info = get_task_set_info(tasks_name_list, app_name2period, data_folder_path)
+        sp_value_list = get_sp_value_list(tasks_name_list, tasks_name_to_info, horizon, horizon_granularity, discard_early_time, task_set_abs_path=task_set_config)
+        x_axis = [i for i in range(0, len(sp_value_list)*horizon_granularity, horizon_granularity)]
+        plt.plot(x_axis, sp_value_list, label = method_name)
+        print(f"SP-Metric for {method_name}: {sum(sp_value_list)/len(sp_value_list)}")
+
+    plt.legend(data_folder_paths.keys())
+    plt.xlabel("Time (s)")
+    plt.ylabel("SP-Metric")
+    plt.tight_layout()
+    
+    plt.savefig(os.path.join(
+        os.path.dirname(OPT_SP_PROJECT_PATH), "all_time_records", "current_scheduler_SP.pdf"), format='pdf')
+    
+    plt.show(block=False)
+    plt.pause(10)
