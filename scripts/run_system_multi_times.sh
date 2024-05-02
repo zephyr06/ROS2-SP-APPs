5d88911a636d961661dc1449396ec3d9079a46e9
zephyr <sen.zephyr.wang@gmail.com>
2024-05-02 16:52:09 -0400

    add run_system_multi_times

---
+++ scripts/run_system_multi_times.sh
@@ -0,0 +1,22 @@
+### check input validity
+if [ $# -eq 0 ]; then
+    echo "No arguments provided. A scheduler is needed: CFS, RM, optimizerBF, optimizerIncremental"
+    echo "Exiting the script ..."
+    exit 0
+fi
+if [ $1 != "RM" ] && [ $1 != "CFS" ] && [ $1 != "optimizerBF" ] && [ $1 != "optimizerIncremental" ] ; then
+    echo "Scheduler $1 is not supported."
+    echo "Supported schedulers are: CFS, RM, optimizerBF, optimizerIncremental"
+    echo "Exiting the script ..."
+    exit 0
+fi
+
+num_iterations=10
+
+for ((i=1; i<=$num_iterations; i++))
+do
+    ./start_system.sh $1
+    # You can put any commands or operations you want to execute in each iteration here
+done
+
+echo "Loop finished"
