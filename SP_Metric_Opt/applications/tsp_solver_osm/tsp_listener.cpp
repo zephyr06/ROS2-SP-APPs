c4776af2c823c9aa01d42c9d5e82ee0e7006f4d0
zephyr <sen.zephyr.wang@gmail.com>
2024-05-07 11:25:29 -0400

    add tsp listener

---
+++ SP_Metric_Opt/applications/tsp_solver_osm/tsp_listener.cpp
@@ -0,0 +1,29 @@
+#include <cassert>
+
+#include "sources/TaskModel/DAG_Model.h"
+#include "sources/Utils/Parameters.h"
+#include "sources/UtilsForROS2/Publisher.h"
+#include "tsp_osm_main_utils.h"
+
+class TSPApp : public AppBase {
+   public:
+    TSPApp() : AppBase("TSP") {}
+    void run(int) override { run_tsp_osm(); }
+};
+
+int main(int argc, char* argv[]) {
+    SP_OPT_PA::DAG_Model dag_tasks = SP_OPT_PA::ReadDAG_Tasks(
+        GlobalVariables::PROJECT_PATH +
+        "../all_time_records/task_characteristics.yaml");
+
+    int index = 0;
+
+    assert(dag_tasks.tasks[index].name == "TSP");
+    double period = dag_tasks.tasks[index].period;
+    int count = dag_tasks.tasks[index].total_running_time / period;
+    TSPApp app;
+    PeriodicReleaser<TSPApp> releaser(period, count, app);
+    releaser.release();
+    exit(0);
+    return 0;
+}
