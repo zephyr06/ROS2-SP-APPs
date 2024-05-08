fe43193cad60be9724ceb1a233e0e410d739ff62
zephyr <sen.zephyr.wang@gmail.com>
2024-05-07 20:00:15 -0400

    add one comment

--- SP_Metric_Opt/Visualize_SP_Metric/test_TaskInfo.py
+++ SP_Metric_Opt/Visualize_SP_Metric/test_TaskInfo.py
@@ -88,6 +88,7 @@ def test_get_sp_value_list():
     sp_value_list = get_sp_value_list(tasks_name_list, tasks_name_to_info, 1000, 10, 0, task_set_config)
     # only one value; chance of four tasks to miss DDL:
     # TSP: 0.0, RRT: 1.0, SLAM: 1.0, MPC: 0.2
+    # TSP's performance term is 0.533333
     exp_sp = (1+1)*interpolate_sp_for_test(-0.01*np.exp(10*0.5)) + interpolate_sp_for_test(np.log(1.5))*(0.5333+1)
     assert sp_value_list == pytest.approx([exp_sp], abs=1e-2)
 
