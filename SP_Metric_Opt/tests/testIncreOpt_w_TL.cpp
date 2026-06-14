091446b5c83757df21b6b0094b8c32cb561b3438
zephyr06 <sen.zephyr.wang@gmail.com>
2026-06-14 18:05:52 -0400

    save some unknkown changes that need to verify later

--- SP_Metric_Opt/tests/testIncreOpt_w_TL.cpp
+++ SP_Metric_Opt/tests/testIncreOpt_w_TL.cpp
@@ -3,10 +3,9 @@
 #include "gmock/gmock.h"  // Brings in gMock.
 #include "sources/Optimization/OptimizeSP_BF.h"
 #include "sources/Optimization/OptimizeSP_TL_Incre.h"
-#include "sources/Utils/Parameters.h"
-
-#include "sources/TaskModel/RegularTasks.h"
 #include "sources/Safety_Performance_Metric/Probability.h"
+#include "sources/TaskModel/RegularTasks.h"
+#include "sources/Utils/Parameters.h"
 
 using ::testing::AtLeast;  // #1
 using ::testing::Return;
@@ -37,7 +36,7 @@ class TaskSetForTest_robotics_v20 : public ::testing::Test {
     void SetUp() override {
         std::string file_name = "test_robotics_v20";
         std::string path =
-            GlobalVariables::PROJECT_PATH + "TaskData/" + file_name + ".yaml";        
+            GlobalVariables::PROJECT_PATH + "TaskData/" + file_name + ".yaml";
         file_path = path;
         dag_tasks = ReadDAG_Tasks(path, 5);
         sp_parameters = SP_Parameters(dag_tasks);
@@ -54,10 +53,11 @@ class TaskSetForTest_taskset_cfg_10_1_gen_1 : public ::testing::Test {
    public:
     void SetUp() override {
         std::string file_name = "taskset_characteristics_0";
-        std::string path =
-            GlobalVariables::PROJECT_PATH + "TaskData/taskset_cfg_10_1_gen_1/" + file_name + ".yaml";
+        std::string path = GlobalVariables::PROJECT_PATH +
+                           "TaskData/taskset_cfg_10_1_gen_1/" + file_name +
+                           ".yaml";
         file_path = path;
-        //dag_tasks = ReadDAG_Tasks(path, 5);
+        // dag_tasks = ReadDAG_Tasks(path, 5);
         dag_tasks = ReadDAG_Tasks(path);
         sp_parameters = SP_Parameters(dag_tasks);
     }
@@ -73,12 +73,14 @@ class TaskSetForTest_taskset_cfg_4_5_gen_1 : public ::testing::Test {
    public:
     void SetUp() override {
         std::string file_name = "taskset_characteristics_0";
-        std::string path =
-            GlobalVariables::PROJECT_PATH + "TaskData/taskset_cfg_4_5_gen_1/" + file_name + ".yaml";
+        std::string path = GlobalVariables::PROJECT_PATH +
+                           "TaskData/taskset_cfg_4_5_gen_1/" + file_name +
+                           ".yaml";
         file_path = path;
-        //dag_tasks = ReadDAG_Tasks(path, 5);
+        // dag_tasks = ReadDAG_Tasks(path, 5);
         dag_tasks = ReadDAG_Tasks(path);
-        sp_parameters = SP_Parameters(dag_tasks);
+        // sp_parameters = SP_Parameters(dag_tasks);
+        sp_parameters = ReadSP_Parameters(path);
     }
 
     // data members
@@ -197,82 +199,89 @@ TEST_F(TaskSetForTest_robotics_v19, optimize_incremental) {
 }
 
 TEST_F(TaskSetForTest_taskset_cfg_10_1_gen_1, optimize_incremental) {
-    OptimizePA_Incre_with_TimeLimits opt(dag_tasks,sp_parameters);  
+    OptimizePA_Incre_with_TimeLimits opt(dag_tasks, sp_parameters);
 
-    int n =GlobalVariables::Layer_Node_During_Incremental_Optimization;
-    std::cout<<"Layer_Node_During_Incremental_Optimization:" << n << std::endl;    
+    int n = GlobalVariables::Layer_Node_During_Incremental_Optimization;
+    std::cout << "Layer_Node_During_Incremental_Optimization:" << n
+              << std::endl;
     auto start_time = CurrentTimeInProfiler;
     opt.OptimizeFromScratch_w_TL(n);
     ResourceOptResult res_opt = opt.CollectResults();
     auto finish_time = CurrentTimeInProfiler;
     double time_taken = GetTimeTaken(start_time, finish_time);
-    std::cout<<"time taken for OptimizeFromScratch_w_TL:" << time_taken << std::endl;
-
-    
+    std::cout << "time taken for OptimizeFromScratch_w_TL:" << time_taken
+              << std::endl;
 
     start_time = CurrentTimeInProfiler;
     opt.OptimizeIncre_w_TL(dag_tasks, n);
     finish_time = CurrentTimeInProfiler;
     time_taken = GetTimeTaken(start_time, finish_time);
-    std::cout<<"time taken for OptimizeIncre_w_TL:" << time_taken << std::endl;
+    std::cout << "time taken for OptimizeIncre_w_TL:" << time_taken
+              << std::endl;
 }
 
 TEST_F(TaskSetForTest_taskset_cfg_4_5_gen_1, check_id2time_limit) {
     // simulate from 0 to 300s, cpu util should increase and then descrease
-	// one task_characteristics file for every 10s 
+    // one task_characteristics file for every 10s
     // check task 3 (with performance_records_time) exetime
-    int task_wPerf = 3; // this task has performance_records_time
-    
-    std::cout<<"\n-------- check id2time_limit varies with cpu utilization\n";    
-    
-    
-    OptimizePA_Incre_with_TimeLimits opt(dag_tasks,sp_parameters);  
+    int task_wPerf = 3;  // this task has performance_records_time
+
+    std::cout << "\n-------- check id2time_limit varies with cpu utilization\n";
+
+    OptimizePA_Incre_with_TimeLimits opt(dag_tasks, sp_parameters);
     int n = GlobalVariables::Layer_Node_During_Incremental_Optimization;
 
-    // time 0: 
+    // time 0:
     opt.OptimizeFromScratch_w_TL(n);
     ResourceOptResult res_opt = opt.CollectResults();
-	// calculate cpu_util for other tasks
+    // calculate cpu_util for other tasks
     double cpu_util = 0.0;
-    for ( int i=0;i<4;i++ ){
-        if (i==task_wPerf) continue;
+    for (int i = 0; i < 4; i++) {
+        if (i == task_wPerf)
+            continue;
         const Task t = dag_tasks.GetTask(i);
         int prd = t.period;
         GaussianDist g_et = t.getExecGaussian();
         double mu = g_et.mu;
-        cpu_util += mu/prd;
+        cpu_util += mu / prd;
     }
-	// print cpu_util, and id2time_limit for task 3
-    printf("%02d: cpu_util_for_other_tasks=%.4f, exe_time_for_task_%d=%.4f\n",0, 
-        cpu_util,task_wPerf,res_opt.id2time_limit[task_wPerf]); 
-    
-	// check k = 1 .. 7 against brute-force scheduler
-    double br_rst[] = {-1,90.0,90.0,90.0,80.556,80.556,80.556,80.556};
-	// for the rest 30*10s, print cpu_util and res_opt.id2time_limit[task_wPerf]
-	// we expect res_opt.id2time_limit[task_wPerf] increase/descrease while cpu_util descrease/increase
-    for (int k=1;k<=30;k++) {        
+    // print cpu_util, and id2time_limit for task 3
+    printf("%02d: cpu_util_for_other_tasks=%.4f, exe_time_for_task_%d=%.4f\n",
+           0, cpu_util, task_wPerf, res_opt.id2time_limit[task_wPerf]);
+
+    // check k = 1 .. 7 against brute-force scheduler
+    double br_rst[] = {-1, 90.0, 90.0, 90.0, 80.556, 80.556, 80.556, 80.556};
+    // for the rest 30*10s, print cpu_util and res_opt.id2time_limit[task_wPerf]
+    // we expect res_opt.id2time_limit[task_wPerf] increase/descrease while
+    // cpu_util descrease/increase
+    for (int k = 1; k <= 30; k++) {
         std::string file_name = "taskset_characteristics_" + std::to_string(k);
-        std::string path =
-            GlobalVariables::PROJECT_PATH + "TaskData/taskset_cfg_4_5_gen_1/" + file_name + ".yaml";
-        DAG_Model dag_tasks_updated = ReadDAG_Tasks(path);    
+        std::string path = GlobalVariables::PROJECT_PATH +
+                           "TaskData/taskset_cfg_4_5_gen_1/" + file_name +
+                           ".yaml";
+        DAG_Model dag_tasks_updated = ReadDAG_Tasks(path);
         opt.OptimizeIncre_w_TL(dag_tasks_updated, n);
         res_opt = opt.CollectResults();
-        
+        // res_opt = EnumeratePA_with_TimeLimits(dag_tasks_updated,
+        // sp_parameters);
+
         cpu_util = 0.0;
-        for ( int i=0;i<4;i++ ){
-            if (i==task_wPerf) continue;
+        for (int i = 0; i < 4; i++) {
+            if (i == task_wPerf)
+                continue;
             const Task t = dag_tasks_updated.GetTask(i);
             int prd = t.period;
             GaussianDist g_et = t.getExecGaussian();
             double mu = g_et.mu;
-            cpu_util += mu/prd;
-        }        
-        printf("%02d: cpu_util_for_other_tasks=%.4f, exe_time_for_task_%d=%.4f\n",k, 
-            cpu_util,task_wPerf,res_opt.id2time_limit[task_wPerf]);   
-        if (k>=1 && k<=7) {
-           double v = abs(res_opt.id2time_limit[task_wPerf] - br_rst[k]);
-		   EXPECT_LT(v, 0.001);  // hopefully result == brute-force result
-        }			
+            cpu_util += mu / prd;
+        }
+        printf(
+            "%02d: cpu_util_for_other_tasks=%.4f, exe_time_for_task_%d=%.4f\n",
+            k, cpu_util, task_wPerf, res_opt.id2time_limit[task_wPerf]);
+        if (k >= 1 && k <= 7) {
+            double v = abs(res_opt.id2time_limit[task_wPerf] - br_rst[k]);
+            EXPECT_LT(v, 0.001);  // hopefully result == brute-force result
+        }
     }
 }
 
